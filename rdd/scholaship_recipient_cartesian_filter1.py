2bd8c71776290c3d641055c7d3fd3c7f6b1e3605
pinaki Saha <pinaki.saha86@gmail.com>
2022-05-09 20:43:37 +0530

    test

--- rdd/scholaship_recipient_cartesian_filter1.py
+++ rdd/scholaship_recipient_cartesian_filter1.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
         .map(lambda line: line.split(",")) \
         .map(lambda lst: (int(lst[0]), strtobool(lst[1]), strtobool(lst[2]), strtobool(lst[3]), int(lst[4])))
 
-    demographics_rdd.show()
-    finances_rdd.show()
+    demographics_rdd.take(10)
+    finances_rdd.take(10)
 
 #spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" rdd/scholaship_recipient_cartesian_filter1.py
