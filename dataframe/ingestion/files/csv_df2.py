1dc3f133ed683d966d9feab181fffd699f3598d4
pinaki Saha <pinaki.saha86@gmail.com>
2022-05-21 09:14:11 +0530

    test

--- dataframe/ingestion/files/csv_df2.py
+++ dataframe/ingestion/files/csv_df2.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
 
   features_df = spark.read \
         .option("mode", "DROPMALFORMED") \
-        .option("header", "false") \
+        .option("header", "true") \
         .option("delimiter", ",") \
         .option("inferSchema", "true") \
         .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/features.csv")
