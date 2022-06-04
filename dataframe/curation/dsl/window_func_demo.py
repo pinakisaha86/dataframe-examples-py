05af0f7799dad4007a324349e0a871e16ff83611
pinaki Saha <pinaki.saha86@gmail.com>
2022-06-04 10:34:48 +0530

    test

--- dataframe/curation/dsl/window_func_demo.py
+++ dataframe/curation/dsl/window_func_demo.py
@@ -45,6 +45,14 @@ if __name__ == '__main__':
         .withColumn("RollingAvg", avg("Amount").over(accNumPrev4WindowSpec))\
         .show(20, False)
 
+    accNumPrev4WindowSpec = Window.partitionBy("AccountNumber")\
+        .orderBy("Date")
+
+    financeDf\
+        .withColumn("Date", to_date(from_unixtime(unix_timestamp("Date", "MM/dd/yyyy"))))\
+        .withColumn("RollingAvg", avg("Amount").over(accNumPrev4WindowSpec))\
+        .show(20, False)
+
     productList = [
         Product("Thin", "Cell phone", 6000),
         Product("Normal", "Tablet", 1500),
