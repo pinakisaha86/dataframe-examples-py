7beadede9b4460a8a0cbc497c6654e26fac0423e
pinaki Saha <pinaki.saha86@gmail.com>
2022-07-05 22:35:41 +0530

    test

--- dataframe/curation/sql/spark_sql_prac.py
+++ dataframe/curation/sql/spark_sql_prac.py
@@ -40,11 +40,11 @@ if __name__ == '__main__':
    # finance_df.orderBy("Date").show(6)
    # finance_df.groupBy("AccountNumber").orderBy("Date").show(6)
 
-finance_df= spark.sql("select * from finances order by amount LIMIT 5").show()
+#finance_df= spark.sql("select * from finances order by amount LIMIT 5").show()
 
-finance_df=spark.sql("select count(*) from finances").show()
-
-#finance_df.repartition(1).write.mode("overwrite").option("header","true").option("delimiter","|").csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/fin_out2")
+#finance_df=spark.sql("select count(*) from finances").show()
+finance_df= spark.sql("select * from finances order by amount")
+finance_df.repartition(1).write.mode("overwrite").option("header","true").option("delimiter","|").csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/fin_out3")
 
 
 # spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" dataframe/curation/sql/spark_sql_prac.py
