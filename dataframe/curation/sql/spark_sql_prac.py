b88b75c38570fd0bc360ccbf95b9435202c0a0e9
pinaki Saha <pinaki.saha86@gmail.com>
2022-07-05 20:22:26 +0530

    test

--- dataframe/curation/sql/spark_sql_prac.py
+++ dataframe/curation/sql/spark_sql_prac.py
@@ -38,8 +38,9 @@ if __name__ == '__main__':
     finance_df.show(5, False)
 
    # finance_df.orderBy("Date").show(6)
-    finance_df.groupBy("AccountNumber").orderBy("Date").show(6)
+   # finance_df.groupBy("AccountNumber").orderBy("Date").show(6)
 
+spark.sql("select * from finances order by amount").show(5, False)
 
 
 
