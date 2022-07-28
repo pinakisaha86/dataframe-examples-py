6603c8d84125e67e827788c9436c78c595f74ddd
pinaki Saha <pinaki.saha86@gmail.com>
2022-07-28 11:36:14 +0530

    test

--- dataframe/curation/sql/broadcast_prac.py
+++ dataframe/curation/sql/broadcast_prac.py
@@ -59,5 +59,5 @@ spark.sql("select a.*, b.* from emp a inner join role b on a.id = b.id").show()
  #   emp_df.join(role_df, [emp_df["id"] == role_df["id"]], "left_anti").show()
  #   emp_df.join(role_df, [emp_df["id"] == role_df["id"]], "full").show()
 
-
+spark.sql('select a*, b* from emp a inner join role b on a.id = b.id').show(5, False)
 # spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" dataframe/curation/sql/broadcast_prac.py
