5ac0a180270a2d486b85fc5b563c360201bc9c96
pinaki Saha <pinaki.saha86@gmail.com>
2022-07-28 10:38:36 +0530

    test

---
+++ dataframe/curation/sql/broadcast_prac.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import broadcast
+from model.Role import Role
+from model.Employee import Employee
+
+if __name__ == '__main__':
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("DSL examples") \
+        .master('local[*]') \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+data1 = [{'Name':'Jhon','ID':21.528,'Add':'USA'},{'Name':'Joe','ID':3.69,'Add':'USA'},
+         {'Name':'Tina','ID':2.48,'Add':'IND'},{'Name':'Jhon','ID':22.22, 'Add':'USA'},
+         {'Name':'Joe','ID':5.33,'Add':'INA'}]
+
+a = sc.parallelize(data1)
+
+b = spark.createDataFrame(a)
+b.show()
+
+# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" dataframe/curation/sql/broadcast_prac.py
